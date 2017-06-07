a455bdb8d8b4084eb4f88558ae175f8b4cab5fef
ZintisMay <zbmayk@gmail.com>
2017-06-07 05:59:36 -0400

    starting to add routes

--- src/app/nav-hamburger/nav-hamburger.component.ts
+++ src/app/nav-hamburger/nav-hamburger.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../../classes/menuItem';
+
+const MENUITEMS: MenuItem[] = [
+	{
+		name:"Belief in the Old Gods", 
+		img: "../assets/BITOGtitle.png", 
+		type:"boardGame",
+		pitch: "A game about the Old Gods and the cultists who love them.",
+		time: "20-40 minutes",
+		players: "2-5",
+		categories:["Hand Management", "Secret Scoring", "Module Board", "Variable Player Powers", "Grid Movement"],
+		description:"The Old Gods never truly die, they slumber beath the waves and among the stars. Play as one of the eldrich abominations of the universe, twisting the minds of men to your will. The first Old God to awaken from their slumber will harken the end of days! This game is a re-themeing of Huts into the Lovecraft universe!"
+	},
+	{
+		name:"Belief in the Old Gods", 
+		img: "../assets/BITOGtitle.png", 
+		type:"boardGame",
+		pitch: "A game about the Old Gods and the cultists who love them.",
+		time: "20-40 minutes",
+		players: "2-5",
+		categories:["Hand Management", "Secret Scoring", "Module Board", "Variable Player Powers", "Grid Movement"],
+		description:"The Old Gods never truly die, they slumber beath the waves and among the stars. Play as one of the eldrich abominations of the universe, twisting the minds of men to your will. The first Old God to awaken from their slumber will harken the end of days! This game is a re-themeing of Huts into the Lovecraft universe!"
+	}
+]
 
 @Component({
   selector: 'app-nav-hamburger',
@@ -6,7 +30,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-hamburger.component.css']
 })
 export class NavHamburgerComponent implements OnInit {
-
+  menuItems: MenuItem[] = MENUITEMS;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/site-routing/site-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SiteRoutingRoutingModule } from './site-routing-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SiteRoutingRoutingModule
+  ],
+  declarations: []
+})
+export class SiteRoutingModule { }
